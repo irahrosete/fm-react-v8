bdb558ed60993479fc8c62d15c98f0d0a021eeba
irah rosete <irah.rosete@outlook.com>
2023-03-31 22:12:34 +1100

    apply useEffect to fetch pets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { createRoot } from "react-dom/client"
-import SearchParams from "./SearchParams"
+import { createRoot } from 'react-dom/client'
+import SearchParams from './SearchParams'
 
 const App = () => {
   return (
@@ -10,6 +10,6 @@ const App = () => {
   )
 }
 
-const container = document.getElementById("root")
+const container = document.getElementById('root')
 const root = createRoot(container)
 root.render(<App />)
