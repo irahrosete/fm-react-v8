f1f5de2747a5a54b76a171f651aacd318dca712d
irah rosete <irah.rosete@outlook.com>
2023-03-31 16:22:43 +1100

    use props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement(
     "div",
     {},
     [
-      React.createElement("h1", {}, "Levi"),
-      React.createElement("h2", {}, "Dog"),
-      React.createElement("h2", {}, "Pug"),
+      React.createElement("h1", {}, props.name),
+      React.createElement("h2", {}, props.animal),
+      React.createElement("h2", {}, props.breed),
     ]
   )
 }
@@ -16,9 +16,21 @@ const App = () => {
     {},
     [
       React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet),
+      React.createElement(Pet, {
+        name: "Levi",
+        animal: "Dog",
+        breed: "Pug"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        name: "Doink",
+        animal: "Cat",
+        breed: "Mixed"
+      }),
     ]
   )
 }
