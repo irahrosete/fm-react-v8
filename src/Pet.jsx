b2d4f3bc77cfd7daa75deeaf27d3c6f285f96bc0
irah rosete <irah.rosete@outlook.com>
2023-03-31 23:46:25 +1100

    style Pet component

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,11 +1,27 @@
-const Pet = (props) => {
-  const { name, animal, breed } = props
+const Pet = ({ id, name, animal, breed, images, location }) => {
+  let hero = 'http://pets-images.dev-apis.com/pets/none.jpg'
+  if (images.length) {
+    hero = images[0]
+  }
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a
+      href={`/details/${id}`}
+      className='pet'
+    >
+      <div className='image-container'>
+        <img
+          src={hero}
+          alt={name}
+        />
+      </div>
+      <div className='info'>
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </a>
   )
 }
 
